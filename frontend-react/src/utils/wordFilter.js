99f7f804323f879bda5d654e6354af2ac543e8cc
Rasb <chilenorasb@gmail.com>
2024-11-11 15:21:45 -0300

    fix en filtro de palabras prohibidas.

--- frontend-react/src/utils/wordFilter.js
+++ frontend-react/src/utils/wordFilter.js
@@ -1,25 +1,25 @@
 // definicion palabras prohibidas
 const prohibitedWords = [
-    'Manzana',
-    'coliflor',
-    'bombilla',
-    'derecha',
-    'izquierda', 
-    'rojo',
-    'azul'
-   ];
-   
-   //reemplazo cada caracter por * para mantener su longitud
-   const ofuscateWord = (word) => '*'.repeat(word.length);
-   
-   const filterComment = (comment) => {
-    let filteredComment = comment;
-    prohibitedWords.forEach(word => {
-      const regex = new RegExp(word, 'gi');
-      filteredComment = filteredComment.replace(regex, ofuscateWord(word));
-    });
-    
-    return filteredComment;
-   };
-   
-   export default filterComment;
+  'Manzana',
+  'coliflor',
+  'bombilla',
+  'derecha',
+  'izquierda', 
+  'rojo',
+  'azul'
+];
+
+const ofuscateWord = (word) => '*'.repeat(word.length);
+
+const filterComment = (comment) => {
+  let filteredComment = comment;
+  prohibitedWords.forEach(word => {
+    // fix: Usamos word boundary (\b) para matchear palabras completas
+    const regex = new RegExp(`\\b${word}\\b`, 'gi');
+    filteredComment = filteredComment.replace(regex, ofuscateWord(word));
+  });
+  
+  return filteredComment;
+};
+
+export default filterComment;
